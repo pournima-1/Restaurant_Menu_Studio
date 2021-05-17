de1b6180d76b170bd6735cb807814546fbc600ec
Pournima Rajagopal <pourni.r@gmail.com>
2021-05-16 21:25:06 -0500

    validation commit

--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -5,6 +5,7 @@ public class MenuItem {
     private double price;
     private String category;
     private String lastUpdated;
+    private String description;
 
     public String getCategory() {
         return category;
@@ -15,11 +16,15 @@ public class MenuItem {
     public String getLastUpdated() {
         return lastUpdated;
     }
+    public String getDescription() {
+        return description;
+    }
 
     public void setCategory(String inputCategory) {this.category = inputCategory;}
     public void setPrice(double inputPrice) {
         this.price = inputPrice;
     }
     public void setLastUpdated(String inputDate) {this.lastUpdated = inputDate;}
+    public void setDescription(String inputDescription) {this.description = inputDescription;}
 }
 
--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -8,12 +8,14 @@ import java.util.Date;
 
 
 public class Restaurant {
-    public static ArrayList<Menu> arrMenu = new ArrayList<Menu>();
-    public static ArrayList<String> arrStringMenu = new ArrayList<String>();
+    public static ArrayList<Menu> arrMenu = new ArrayList<>();
+    public static ArrayList<String> arrStringMenu = new ArrayList<>();
 
     public static void main(String[] args){
         String strAddDecision = "";
-        String strRemoveDecision = "";
+        String strRemoveDecision;
+
+        String str1;
 
         do{
             Scanner input = new Scanner(System.in);
@@ -23,15 +25,21 @@ public class Restaurant {
                 System.out.println("This menu alredy exists. Enter new menu");
             }
             else {
-                System.out.println("Enter price: ");
-                double dPrice = input.nextDouble();
+                do {
+                    System.out.println("Enter price in numbers: ");
+                    str1 = input.nextLine();
+                }while(!str1.matches("^[0.0-9]+$"));
+                double dPrice = Double.parseDouble(str1);
                 System.out.println("Enter category: ");
                 Scanner input1 = new Scanner(System.in);
                 String strCategory = input1.nextLine();
+                System.out.println("Enter description: ");
+                Scanner input7 = new Scanner(System.in);
+                String strDescription = input7.nextLine();
                 System.out.println("Enter menu date(YYYY-MM-DD): ");
                 Scanner input5 = new Scanner(System.in);
                 String strDate = input5.nextLine();
-                addMenu(strMenuName, dPrice, strCategory, strDate);
+                addMenu(strMenuName, dPrice, strCategory, strDate, strDescription);
                 System.out.println("Do you want to add another menu?(Y/N): ");
                 Scanner input2 = new Scanner(System.in);
                 strAddDecision = input2.nextLine();
@@ -57,12 +65,13 @@ public class Restaurant {
         displayMenu();
     }
 
-    public static void addMenu(String menuName, double price, String category, String menuDate)
+    public static void addMenu(String menuName, double price, String category, String menuDate, String desc)
     {
         Menu objMenu = new Menu(menuName);
         objMenu.setPrice(price);
         objMenu.setCategory(category);
         objMenu.setLastUpdated(menuDate);
+        objMenu.setDescription(desc);
         arrMenu.add(objMenu);
         arrStringMenu.add(menuName);
     }
@@ -70,9 +79,9 @@ public class Restaurant {
     public static void removeMenu(String menuName)
     {
         int iIndex = arrStringMenu.indexOf(menuName);
-        System.out.println(arrMenu);
-        System.out.println(arrMenu.size());
-        System.out.println(iIndex);
+        //System.out.println(arrMenu);
+        //System.out.println(arrMenu.size());
+       // System.out.println(iIndex);
         arrMenu.remove(iIndex);
     }
 
@@ -83,10 +92,11 @@ public class Restaurant {
             System.out.println("Menu " + (i+1) + " Name: " + arrMenu.get(i).getMenu());
             System.out.println("Menu " + (i+1) + " Price: " + arrMenu.get(i).getPrice());
             System.out.println("Menu " + (i+1) + " Category: " + arrMenu.get(i).getCategory());
+            System.out.println("Menu " + (i+1) + " Description: " + arrMenu.get(i).getDescription());
             System.out.println("Menu " + (i+1) + " LastUpdated: " + arrMenu.get(i).getLastUpdated());
             LocalDate dDate = LocalDate.parse(arrMenu.get(i).getLastUpdated());
-            System.out.println(dDate);
-            System.out.println(LocalDate.now());
+           // System.out.println(dDate);
+            //System.out.println(LocalDate.now());
            if(dDate.equals(LocalDate.now()))
             {
                 System.out.println("Menu " + (i+1) + "New Menu");
